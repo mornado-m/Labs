698435f2b569a51f215f6b7d54b4b4d8ae0f4c30
mornado <mornado12@mail.ru>
2017-03-14 10:53:52 +0200

    Lab_3 done

---
+++ Lab_3/Example/jquery.reversetext.js
@@ -0,0 +1,39 @@
+(function($) {
+
+    $.fn.reverseText = function(params) {  
+
+        // merge default and user parameters  
+
+        params = $.extend( {minlength: 0, maxlength: 99999}, params);  
+
+     
+
+        this.each(function() {     
+
+     
+
+            var $t = $(this);  
+
+
+
+            var origText = $t.text();
+
+            var newText = '';  
+
+                if (origText.length >= params.minlength &&  origText.length <= params.maxlength) { 
+
+                    for (var i = origText.length-1; i >= 0; i--) 
+
+                        newText += origText.substr(i, 1);  
+
+                    $t.text(newText);
+
+                }
+
+        });  
+
+        return this;  
+
+    }; 
+
+})(jQuery);
